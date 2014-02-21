565db0c8118365402155406341e0e91d9887f2a1
bvasseur <bvasseur@siliconsalad.com>
2014-02-21 12:15:43 +0100

    Cross domain request
    Marche sous Chrome, FF, Safari, Opera, IE 11, 10, 9, 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*jslint browser:true */
 /*jslint devel: true */
+/*global XDomainRequest: false*/
 
 (function () {
     'use strict';
@@ -21,45 +22,103 @@
         }
     }
     
-    document.addEventListener("DOMContentLoaded", function (event) {
-        var url = 'test.json';
+    function ieSendRequestCrossDomain(url, idDomResult) {
+        var xdr = new XDomainRequest();
+        xdr.open('get', url);
+        xdr.onload = function () {
+            console.log('XDR onlaod');
+            console.log('request done :)');
+            console.info('response = ' + xdr.responseText);
+            var response = JSON.parse(xdr.responseText);
+            console.log('name = ' + response.name);
+            document.getElementById(idDomResult).innerHTML = xdr.responseText;
+        };
+        xdr.onerror = function () {
+            console.log('error');
+        };
+        xdr.progress = function () {
+            console.log('progress');
+        };
+        xdr.ontimeout = function () {
+            console.log('timeout');
+        };
+        xdr.send();
+    }
+    
+    function sendRequest(url, idDomResult) {
+        var xhr;
+        console.log("prepare to send a request");
 
-        function sendRequest() {
-            var xhr;
-            console.log("prepare to send a request");
-            
-            if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-                xhr = new XMLHttpRequest();
-            } else if (window.ActiveXObject) { // IE
+        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // IE < 10
+            try {
+                xhr = new window.ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
                 try {
-                    xhr = new window.ActiveXObject("Msxml2.XMLHTTP");
-                } catch (e) {
-                    try {
-                        xhr = new window.ActiveXObject("Microsoft.XMLHTTP");
-                    } catch (ex) {}
-                }
+                    xhr = new window.ActiveXObject("Microsoft.XMLHTTP");
+                } catch (ex) {}
             }
-            
-            
-            xhr.open('GET', url);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    console.log('request done :)');
-                    console.info('response = ' + xhr.responseText);
-                    var response = JSON.parse(xhr.responseText);
-                    console.log('name = ' + response.name);
-                    document.getElementById("result").innerHTML = xhr.responseText;
-                } else if (xhr.readyState === xhr.DONE && xhr.status !== 200) {
-                    console.error('Erreur : ' + 'Code = ' + xhr.status + ' texte = ' + xhr.statusText);
-                }
-            };
-            xhr.send(null);
         }
 
-        document.getElementById("requete").addEventListener("click", sendRequest);
-    });
-
-	
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log('request done :)');
+                console.info('response = ' + xhr.responseText);
+                var response = JSON.parse(xhr.responseText);
+                console.log('name = ' + response.name);
+                document.getElementById(idDomResult).innerHTML = xhr.responseText;
+            } else if (xhr.readyState === xhr.DONE && xhr.status !== 200) {
+                console.error('Erreur : ' + 'Code = ' + xhr.status + ' texte = ' + xhr.statusText);
+            }
+        };
+        xhr.send(null);
+    }
+    
+    function initApp() {
+        var local_url = 'test.json',
+            webPuzzleUrl = 'http://webpuzzlews.herokuapp.com/web_components/AngularJsWc.json',
+            github_url = 'https://api.github.com/users/siliconsalad',
+            idDomResultSameDomain = 'result_same_domain',
+            idDomResultGithub = 'result_github',
+            domElementSameDomain = document.getElementById('requete_same_domain'),
+            domElementGitHub = document.getElementById('requete_github');
+        
+        if (!domElementSameDomain.addEventListener) {
+            domElementSameDomain.attachEvent('onclick', function () {
+                sendRequest(local_url, idDomResultSameDomain);
+            });
+        } else {
+            domElementSameDomain.addEventListener("click", function () {
+                sendRequest(local_url, idDomResultSameDomain);
+            });
+        }
+        
+        if (!domElementGitHub.addEventListener) {
+            domElementGitHub.attachEvent('onclick', function () {
+                ieSendRequestCrossDomain(webPuzzleUrl, idDomResultGithub);
+            });
+        } else {
+            domElementGitHub.addEventListener('click', function () {
+                if (window.hasOwnProperty('XDomainRequest') && window.XDomainRequest !== null) {
+                    ieSendRequestCrossDomain(webPuzzleUrl, idDomResultGithub);
+                } else {
+                    sendRequest(github_url, idDomResultGithub);
+                }
+            });
+        }
+    }
+    
+    document.onreadystatechange = function () {
+        if (document.readyState === "complete") {
+            console.log("state = " + document.readyState);
+            initApp();
+        }
+    };
+    
+//    document.addEventListener("DOMContentLoaded", function (event) {
+        
 }());
 
 
